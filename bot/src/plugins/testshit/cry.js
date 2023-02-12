862b4399359e14aec4ed301dad27cb5e3fb0ea36
ogios <96933655+ogios@users.noreply.github.com>
2023-02-12 14:31:03 +0800

    Update cry.js

--- bot/src/plugins/testshit/cry.js
+++ bot/src/plugins/testshit/cry.js
@@ -3,7 +3,7 @@ const crypto = require("crypto-js");
 
 // var key0 = "L#$@XowPu!uZ&c%u";
 // var iv0 = "2auvLZzxz7bo#^84";
-// var body = "futanari好吗";
+// var body = "futanari太好了";
 // var key = crypto.enc.Latin1.parse(key0);
 // var iv = crypto.enc.Latin1.parse(iv0);
 // var encrypted = crypto.AES.encrypt(body, key, {
@@ -27,5 +27,3 @@ function encode(body){
     // console.log(encrypted)
     return encrypted;
 }
-
-crypto.enc.Latin1
